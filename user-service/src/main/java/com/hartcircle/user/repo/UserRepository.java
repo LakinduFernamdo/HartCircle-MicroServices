9b0b9abdee6e5919ee0c61db5590cab1c977a950
2001lakindu <lakindu72001@gmail.com>
2025-07-31 09:55:56 +0530

    implement forgot password with java JMailSender

--- user-service/src/main/java/com/hartcircle/user/repo/UserRepository.java
+++ user-service/src/main/java/com/hartcircle/user/repo/UserRepository.java
@@ -11,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByNic(String nic);
+    Optional<User> findByEmail(String email);
 }
 
