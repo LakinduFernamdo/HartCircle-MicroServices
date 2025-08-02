b297e6258be1d1c1d6a8be4792d1b475e5a529d2
2001lakindu <lakindu72001@gmail.com>
2025-08-02 09:11:43 +0530

    Removed application.properties and updated .gitignore

---
+++ bid-service/src/main/java/com/hartcircle/bid/BidMain.java
@@ -0,0 +1,12 @@
+package com.hartcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidMain {
+    public static void main(String[] args) {
+        SpringApplication.run(BidMain.class, args);
+        System.out.println("hello");
+    }
+}
