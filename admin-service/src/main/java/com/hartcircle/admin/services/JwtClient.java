2c6192f60a2f8a06a894dc582f42a6a5232feeec
2001lakindu <lakindu72001@gmail.com>
2025-09-09 19:25:07 +0530

    implement login functionality for admin with jwt-authentication

--- admin-service/src/main/java/com/hartcircle/admin/services/JwtClient.java
+++ admin-service/src/main/java/com/hartcircle/admin/services/JwtClient.java
@@ -1,5 +1,7 @@
 package com.hartcircle.admin.services;
 
+import com.hartcircle.admin.dto.JwtRequest;
+import com.hartcircle.admin.dto.JwtResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +18,9 @@ public class JwtClient {
 
     @Value("${user.service.url}")  // from application.properties
     private String userServiceUrl;
+
+    @Value("${jwt.service.url}")
+    private String jwtServiceUrl;
 
     public JwtClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
@@ -36,6 +41,24 @@ public class JwtClient {
 
             return response.getBody();
         } catch (HttpClientErrorException e) {
+            return null;
+        }
+    }
+
+    public String generateToken(String nic) {
+        try {
+            JwtRequest request = new JwtRequest();
+            request.setUserNIC(nic);
+
+            ResponseEntity<JwtResponse> response = restTemplate.postForEntity(
+                    jwtServiceUrl + "/jwt/generate",
+                    request,
+                    JwtResponse.class
+            );
+
+            return response.getBody() != null ? response.getBody().getToken() : null;
+        } catch (Exception e) {
+            System.out.println("Token generation failed: " + e.getMessage());
             return null;
         }
     }
