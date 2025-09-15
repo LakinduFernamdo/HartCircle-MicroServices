d0747ce0b8b38bfc513d5540231e68b8caf70825
2001lakindu <lakindu72001@gmail.com>
2025-09-15 19:25:16 +0530

    create classes for render pos data by NIC

---
+++ admin-service/src/main/java/com/hartcircle/admin/services/GetPostDataService.java
@@ -0,0 +1,5 @@
+package com.hartcircle.admin.services;
+
+public class GetPostDataService {
+    //This will render post-owner = entered NIC
+}
---
+++ post-service/src/main/java/com/hartcircle/post/dto/PostContent.java
@@ -0,0 +1,5 @@
+package com.hartcircle.post.dto;
+
+//This will include which need render post data to see admin
+public class PostContent {
+}
