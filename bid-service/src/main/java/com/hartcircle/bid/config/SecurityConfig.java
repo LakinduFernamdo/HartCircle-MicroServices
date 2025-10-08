c77d792a5b5cc9dc35236dcabb99dc6cbbf91902
2001lakindu <lakindu72001@gmail.com>
2025-10-08 08:48:43 +0530

    debug bid-service security changes

--- bid-service/src/main/java/com/hartcircle/bid/config/SecurityConfig.java
+++ bid-service/src/main/java/com/hartcircle/bid/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/api/v1/bid/**").authenticated()
